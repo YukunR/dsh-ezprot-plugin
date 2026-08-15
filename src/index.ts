60af5d442656aa5299741fe529aa58baa005ac86
yukunr <14616586+yukun-r@user.noreply.gitee.com>
2026-08-15 20:00:10 +0800

    chore: default dockerImage to the 0.1.0-beta pre-release tag

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ export const Config = Schema.object({
   enableInstall: Schema.boolean().default(true),
   defaultTimeoutMs: Schema.number().default(1800000),
   backend: Schema.string().default('auto'),
-  dockerImage: Schema.string().default('yukunru/ezprot:latest'),
+  dockerImage: Schema.string().default('yukunru/ezprot:0.1.0-beta'),
 })
 
 const PROMPT_SECTION = [
